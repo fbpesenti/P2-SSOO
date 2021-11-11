db58a7994bb717bcb5bb7b13ab731886a32050dd
mpsubiabre <you@example.com>
2021-11-11 09:27:44 -0300

    menu funcional

--- client/src/function.h
+++ client/src/function.h
@@ -1,7 +1,11 @@
 
-char  crear_aldeano();
-void principal_menu();
-char subir_nivel();
-char * robar();
-char espiar();
-char atacar();
+#include <unistd.h>
+#include <stdio.h>
+#include <stdbool.h> 
+
+char menu_crear_aldeano();
+bool principal_menu(int server_socket);
+char menu_subir_nivel();
+char * menu_robar();
+char menu_espiar();
+char menu_atacar();
